d3ebec1ccd30a2c76dc14f13a4c4d65da8377b23
Tommy Ivarsson <tomiva@avensia.com>
2015-11-22 19:22:09 +0100

    Init commit

---
+++ EpiserverMasterClass/Business/IModifyLayout.cs
@@ -0,0 +1,13 @@
+using EpiserverMasterClass.Models.ViewModels;
+
+namespace EpiserverMasterClass.Business
+{
+    /// <summary>
+    /// Defines a method which may be invoked by PageContextActionFilter allowing controllers
+    /// to modify common layout properties of the view model.
+    /// </summary>
+    interface IModifyLayout
+    {
+        void ModifyLayout(LayoutModel layoutModel);
+    }
+}
