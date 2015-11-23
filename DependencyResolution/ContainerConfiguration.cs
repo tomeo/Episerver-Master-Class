430889f6e3a642177df386efddd2c665b3abf57c
Tommy Ivarsson <tommy.ivarsson@gmail.com>
2015-11-23 12:50:52 +0100

    Added Depedency Resolution

---
+++ DependencyResolution/ContainerConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.ServiceLocation;
+
+namespace DependencyResolution
+{
+    [ModuleDependency(typeof(ServiceContainerInitialization))]
+    [InitializableModule]
+    public class ContainerConfiguration : IConfigurableModule
+    {
+        public void ConfigureContainer(ServiceConfigurationContext context)
+        {
+            context.Container.Configure(x =>
+            {
+
+            });
+
+            DependencyResolver.SetResolver(new StructureMapDependencyResolver(context.Container));
+        }
+
+        public void Initialize(InitializationEngine context) { }
+        public void Uninitialize(InitializationEngine context) { }
+        public void Preload(string[] parameters) { }
+    }
+}
